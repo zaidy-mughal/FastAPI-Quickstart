687b27569c77931373de6b633d45565db76c020c
zaidy-mughal <zaidamjadse@gmail.com>
2026-01-15 23:00:11 +0500

    [FEAT]: added more code by reading official docs

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 from fastapi import FastAPI
+from fastapi import Query
+from typing import Optional, Annotated
+from pydantic import BaseModel
+
 
 app = FastAPI()
 
@@ -9,5 +13,40 @@ async def root():
 
 
 @app.get("/hello/{name}")
-async def say_hello(name: str):
+async def say_hello(name: int):
     return {"message": f"Hello {name}"}
+
+
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+
+@app.get("/items/")
+async def read_item(skip: Optional[int] = None, limit: Optional[int]= None):
+    if skip is None:
+        skip = 0
+    if limit is None:
+        limit = 1
+    return fake_items_db[skip: skip + limit]
+
+
+class Item(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    tax: float | None = None
+
+
+@app.put("/items/{item_id}")
+async def update_item(item_id: int, item: Item, q: str | None = None):
+    result = {"item_id": item_id, **item.model_dump()}
+    if q:
+        result.update({"q": q})
+    return result
+
+
+# additional validation
+@app.get("/products/")
+async def read_items(q: Annotated[str | None, Query(max_length=50)] = None):
+    results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
+    if q:
+        results.update({"q": q})
+    return results
